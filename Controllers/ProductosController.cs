81fa03a32e7aa93c67acb592e2aeeb9d78ab2155
tincho020602 <ezequiel.ibarra020602@gmail.com>
2024-11-01 20:10:51 -0300

    segundo commit, metodo para obtener todos los productos y un html listos

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -4,29 +4,30 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using tl2_tp6_2024_GonEnGit.Models;
 
+using EspacioModelos;
+using EspacioRepositorios;
+
 namespace tl2_tp6_2024_GonEnGit.Controllers;
 
 public class ProductosController : Controller
 {
     private readonly ILogger<ProductosController> _logger;
+    private ProductoRepository repoProducto;
 
     public ProductosController(ILogger<ProductosController> logger)
     {
         _logger = logger;
+        repoProducto = new ProductoRepository();
     }
 
 // todas la views van a ser archivos dentro de Views,
 // aquí pones una metodo que llame al nombre del archivo
 // y retorne el metodo View()
+// dentro de estos metodos usas los controllers de cada cosa
     public IActionResult Index()
     {
-        return View();
-    }
-
-// dentro de estos modesl haces los controllers de cada cosa
-    public IActionResult Privacy()
-    {
-        return View();
+        List<Productos> lista = repoProducto.ListarProductos();
+        return View(lista);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
